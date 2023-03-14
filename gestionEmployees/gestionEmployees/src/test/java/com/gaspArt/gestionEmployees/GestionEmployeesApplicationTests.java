b4debd72ef50238f7f44af4c55f896463264d3d0
Hgaspard <95709246+Hgaspard@users.noreply.github.com>
2023-03-14 22:24:09 +0000

    Add files via upload

---
+++ gestionEmployees/gestionEmployees/src/test/java/com/gaspArt/gestionEmployees/GestionEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspArt.gestionEmployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
